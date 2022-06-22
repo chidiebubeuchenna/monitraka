338dc6cb37da6d40a8d35a5bca5cce52fae0a02d
Olaoluwa <tokiolaoluwa01@gmail.com>
2022-06-22 15:35:32 +0100

    drop down menu

--- lib/widgets/drop_down_menu.dart
+++ lib/widgets/drop_down_menu.dart
@@ -11,23 +11,65 @@ class DropDown extends StatefulWidget {
 class _DropDownState extends State<DropDown> {
   late int selectedRadio;
   late int selectedRadioTile;
+
+  // List<DropdownMenuItem<int>> get dropdownItems {
+  //   List<DropdownMenuItem<int>> menuItems = [
+  //     DropdownMenuItem(value: 1, child: item(1, 1, 'WhatsApp')),
+  //     DropdownMenuItem(value: 2, child: item(2, 2, 'Twitter')),
+  //     DropdownMenuItem(value: 3, child: item(3, 3, 'Instagram')),
+  //     DropdownMenuItem(value: 4, child: item(4, 4, 'A friend')),
+  //     DropdownMenuItem(value: 5, child: item(5, 5, 'Our team')),
+  //   ];
+  //   return menuItems;
+  // }
+
   List<DropdownMenuItem<String>> get dropdownItems {
     List<DropdownMenuItem<String>> menuItems = [
-      const DropdownMenuItem(value: "USA", child: Text("USA")),
-      const DropdownMenuItem(value: "Canada", child: Text("Canada")),
-      const DropdownMenuItem(value: "Brazil", child: Text("Brazil")),
-      const DropdownMenuItem(value: "England", child: Text("England")),
+      DropdownMenuItem(child: Text("USA"), value: "USA"),
+      DropdownMenuItem(child: Text("Canada"), value: "Canada"),
+      DropdownMenuItem(child: Text("Brazil"), value: "Brazil"),
+      DropdownMenuItem(child: Text("England"), value: "England"),
     ];
     return menuItems;
   }
 
-  RadioListTile item(int value, int groupValue, String title) {
+  form() {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxWidth: 100, maxHeight: 200),
+      child: DropdownButtonFormField(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Resources.color.cGreen),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          filled: true,
+          fillColor: Resources.color.fillColor,
+        ),
+        items: dropdownItems,
+        onChanged: (x) {},
+      ),
+    );
+  }
+
+  button() {
+    return DropdownButton(
+        value: 1,
+        dropdownColor: Colors.green,
+        onChanged: null,
+        items: dropdownItems);
+  }
+
+  item(int value, int groupValue, String title) {
     return RadioListTile(
       value: value,
       groupValue: groupValue,
-      onChanged: (x) {
+      onChanged: (int? value) {
         print('From: $title');
-        selectedTile(x);
+        // selectedTile(xfi);
       },
       title: Text(
         title,
@@ -55,6 +97,19 @@ class _DropDownState extends State<DropDown> {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField(items: dropdownItems, onChanged: (x) {});
+    return demo();
   }
+}
+
+RadioListTile demo() {
+  return RadioListTile(
+    value: 1,
+    groupValue: 1,
+    onChanged: (index) {
+      // setState(() {
+      //   selectedRadioTile
+      // });
+    },
+    title: Text('WhatsApp'),
+  );
 }
