c9bc554ec26c9607604d56bf5b00d144ce60c998
Igbadun Grace <igbaduneunhye@gmail.com>
2022-07-31 13:31:24 +0100

    Update tiles.dart
    created a tile for the contact screen, where icons are all black

--- lib/widgets/tiles.dart
+++ lib/widgets/tiles.dart
@@ -14,7 +14,7 @@ Widget profileTile(IconData tileIcon, String title, void Function() nav) {
           bottomLeft: Radius.circular(4)),
     ),
     child: ListTile(
-      leading: Icon(tileIcon, color: Resources.color.cBlack),
+      leading: Icon(tileIcon, color: Resources.color.cYellow),
       title: Text(
         title,
         style: TextStyle(
@@ -48,6 +48,38 @@ Widget logoutTile(IconData tileIcon, String title, void Function() nav) {
     ),
     child: ListTile(
       leading: Icon(tileIcon, color: Resources.color.redText),
+      title: Text(
+        title,
+        style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+            color: Resources.color.cBlack),
+      ),
+      onTap: nav,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(8),
+            topRight: Radius.circular(8),
+            bottomRight: Radius.circular(8),
+            bottomLeft: Radius.circular(4)),
+      ),
+    ),
+  );
+}
+
+Widget contactTile(IconData tileIcon, String title, void Function() nav) {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      border: Border.all(color: Resources.color.cGreen),
+      borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(8),
+          topRight: Radius.circular(8),
+          bottomRight: Radius.circular(8),
+          bottomLeft: Radius.circular(4)),
+    ),
+    child: ListTile(
+      leading: Icon(tileIcon, color: Resources.color.cBlack),
       title: Text(
         title,
         style: TextStyle(
